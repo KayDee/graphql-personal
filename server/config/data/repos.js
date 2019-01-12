18e8693eb36c7a22589bdbc31b41e1d5c019863b
KaustubhD <kaustubhd287@gmail.com>
2019-01-13 00:37:22 +0530

    After these kind of mistakes, i think i need a break

--- server/config/data/repos.js
+++ server/config/data/repos.js
@@ -1,19 +1,12 @@
-let axios = require('axios')
+const axios = require('axios')
 
 export const repos = axios('https://api.github.com/users/KayDee/repos?type=all&per_page=100')
-.then(data => {
-  // console.log(data)
-  data = data.data.map(repo => ({
+.then(data => data.data.map(repo => ({
     name: repo.name,
     description: repo.description,
     url: repo.html_url,
     language: repo.language,
-    stars: repo.stargazers_count
-  }))
-  // console.log(data)
-  return data
-}).then(res => res.filter(repo => !repo.fork)).catch(err => console.error(err))
-
-// export default REPO_INFO
-
-// console.log(repos)
+    stars: repo.stargazers_count,
+    fork: repo.fork
+  })
+)).then(res => res.filter(repo => !repo.fork)).catch(err => console.error(err))
