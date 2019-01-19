590e122dc2564c93de36688a4130b4594f4db441
KaustubhD <kaustubhd287@gmail.com>
2019-01-19 15:52:45 +0530

    projects

--- server/config/data/projects.js
+++ server/config/data/projects.js
@@ -9,6 +9,42 @@ export const PROJECTS = [
     name: 'Website',
     description: 'A simple personal website in react',
     language: 'Javascript',
-    url: 'https://kaydee.github.io'
+    url: 'https://KayDee.github.io'
   },
+  {
+    name: 'R-F-Auth',
+    description: 'A simple pageset in React with firebase authorisation',
+    language: 'JavaScript',
+    url: 'https://github.com/KayDee/R-F-Auth'
+  },
+  {
+    name: 'Geometrize',
+    description: 'A simple page to convert images to shapes',
+    language: 'JavaScript',
+    url: 'https://github.com/KayDee/Geometrize'
+  },
+  {
+    name: 'TextToEmoji',
+    description: 'A simple app to change your text to 😀 ',
+    language: 'JavaScript',
+    url: 'https://KayDee.github.io/TextToEmoji'
+  },
+  {
+    name: 'playlist-app',
+    description: 'A simple React app with Spotify connections',
+    language: 'JavaScript',
+    url: 'http://spot-playlist-app.herokuapp.com/'
+  },
+  {
+    name: 'CYK',
+    description: 'A simple implementation of CYK algorithm',
+    language: 'Java',
+    url: 'https://github.com/KayDee/CYK-implementation-JAVA'
+  },
+  {
+    name: 'Automaton',
+    description: 'A simple representation of cellular automatons',
+    language: 'JavaScript',
+    url: 'https://github.com/KayDee/Automaton'
+  }
 ]
